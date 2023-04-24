d700398315fe95935acb38352be7fce5c32f475c
Iryna Brusentsova <bio200480@gmail.com>
2023-04-24 22:16:20 +0300

    Add function menu

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// find humburger
+const humburger = document.querySelector('.header-humburger');
+console.log (humburger);
+
+// find menu
+const menu = document.querySelector('.header-grid-menu');
+console.log (menu);
+
+humburger.addEventListener('click', function (){
+    menu.classList.toggle('active');
+})
