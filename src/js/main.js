f0f767cc2865cb65f1896f662e6132d543945e75
Iryna Brusentsova <bio200480@gmail.com>
2023-04-27 13:03:34 +0300

    Add new class to file

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 'use strict'
 // find humburger
 const humburger = document.querySelector('.header-humburger');
-console.log (humburger);
-
 // find menu
 const menu = document.querySelector('.header-grid-menu');
 console.log (menu);
+// find menu
+const body =  document.querySelector('body');
 
 humburger.addEventListener('click', function (){
     menu.classList.toggle('active');
+    body.classList.toggle('lock');
 })
