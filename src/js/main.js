9c6f4d57d944c6e5195e063edd8dbfea56eea6b7
Iryna Brusentsova <bio200480@gmail.com>
2023-06-12 22:27:17 +0300

    Add function button up

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,38 @@ humburger.addEventListener('click', function (){
     menu.classList.toggle('active');
     body.classList.toggle('lock');
 })
+
+
+const btnUp = {
+    el: document.querySelector('.btnUp'),
+    show() {
+      // удалим у кнопки класс btn-up_hide
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      // добавим к кнопке класс btn-up_hide
+      this.el.classList.add('btn-up_hide');
+    },
+   
+    
+    addEventListener() {
+      // при прокрутке содержимого страницы
+      window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 300px, то делаем кнопку видимой, иначе скрываем
+        scrollY > 300 ? this.show() : this.hide();
+      });
+      // при нажатии на кнопку .btn-up
+      document.querySelector('.btnUp').onclick = () => {
+        // переместим в начало страницы
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
